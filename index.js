65e14bb42a0f9ee0d01492f905605a2ba928c380
wonderlexicon <nh756@nyu.edu>
2020-12-19 16:02:28 -0500

    added index.js info for dodec from ngokevin superframe layout

--- index.js
+++ index.js
@@ -37,6 +37,162 @@ server.listen(port,() =>{
   }
 });
 
+AFRAME.registerComponent('layout', {
+  schema: {
+    columns: {default: 1, min: 0, if: {type: ['box']}},
+    margin: {default: 1, min: 0, if: { type: ['box', 'line']}},
+    radius: {default: 1, min: 0, if: {
+      type: ['circle', 'cube', 'dodecahedron', 'pyramid']
+    }},
+    type: {default: 'line', oneOf: [
+      'box', 'circle', 'cube', 'dodecahedron', 'line', 'pyramid'
+    ]}
+  },
+
+/**
+   * Store initial positions in case need to reset on component removal.
+   */
+  init: function () {
+    var self = this;
+    var el = this.el;
+
+    this.children = el.getChildEntities();
+    this.initialPositions = [];
+
+    this.children.forEach(function getInitialPositions (childEl) {
+      if (childEl.hasLoaded) { return _getPositions(); }
+      childEl.addEventListener('loaded', _getPositions);
+      function _getPositions () {
+        var position = childEl.getAttribute('position');
+        self.initialPositions.push([position.x, position.y, position.z]);
+      }
+    });
+
+    el.addEventListener('child-attached', function (evt) {
+      // Only update if direct child attached.
+      if (evt.detail.el.parentNode !== el) { return; }
+      self.children.push(evt.detail.el);
+      self.update();
+    });
+
+    el.addEventListener('child-detached', function (evt) {
+      // Only update if direct child detached.
+      if (self.children.indexOf(evt.detail.el) === -1) { return; }
+      self.children.splice(self.children.indexOf(evt.detail.el), 1);
+      self.initialPositions.splice(self.children.indexOf(evt.detail.el), 1);
+      self.update();
+    });
+  },
+
+  /**
+   * Update child entity positions.
+   */
+
+  update: function (oldData) {
+    var children = this.children;
+    var data = this.data;
+    var el = this.el;
+    var numChildren = children.length;
+    var positionFn;
+    var positions;
+    var startPosition = el.getAttribute('position');
+
+// Calculate different positions based on layout shape.
+switch (data.type) {
+
+case 'dodecahedron': {
+  positionFn = getDodecahedronPositions;
+  break;
+}
+default: {
+        // Line.
+        positionFn = getLinePositions;
+      }
+    }
+positions = positionFn(data, numChildren, startPosition);
+setPositions(children, positions);
+},
+
+  /**
+   * Reset positions.
+   */
+
+  remove: function () {
+    this.el.removeEventListener('child-attached', this.childAttachedCallback);
+    setPositions(this.children, this.initialPositions);
+  }
+});
+
+/**
+ * Get positions for `dodecahedron` layout.
+ */
+function getDodecahedronPositions (data, numChildren, startPosition) {
+  var PHI = (1 + Math.sqrt(5)) / 2;
+  var B = 1 / PHI;
+  var C = 2 - PHI;
+  var NB = -1 * B;
+  var NC = -1 * C;
+
+  return transform([
+    [-1, C, 0],
+    [-1, NC, 0],
+    [0, -1, C],
+    [0, -1, NC],
+    [0, 1, C],
+    [0, 1, NC],
+    [1, C, 0],
+    [1, NC, 0],
+    [B, B, B],
+    [B, B, NB],
+    [B, NB, B],
+    [B, NB, NB],
+    [C, 0, 1],
+    [C, 0, -1],
+    [NB, B, B],
+    [NB, B, NB],
+    [NB, NB, B],
+    [NB, NB, NB],
+    [NC, 0, 1],
+    [NC, 0, -1],
+  ], startPosition, data.radius / 2);
+}
+module.exports.getDodecahedronPositions = getDodecahedronPositions;
+
+/**
+ * Multiply all coordinates by a scale factor and add translate.
+ *
+ * @params {array} positions - Array of coordinates in array form.
+ * @returns {array} positions
+ */
+function transform (positions, translate, scale) {
+  translate = [translate.x, translate.y, translate.z];
+  return positions.map(function (position) {
+    return position.map(function (point, i) {
+      return point * scale + translate[i];
+    });
+  });
+};
+
+
+/**
+ * Set position on child entities.
+ *
+ * @param {array} els - Child entities to set.
+ * @param {array} positions - Array of coordinates.
+ */
+function setPositions (els, positions) {
+  els.forEach(function (el, i) {
+    var position = positions[i];
+    el.setAttribute('position', {
+      x: position[0],
+      y: position[1],
+      z: position[2]
+    });
+  });
+}
+
+
+
 
 window.addEventListener("wheel", event=>{
     let myCamera = document.getElementById("camera");
