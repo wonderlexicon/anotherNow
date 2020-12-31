317c6dde77d3868049b27d1fbb403b43abc9799f
wonderlexicon <nh756@nyu.edu>
2020-12-31 10:20:18 -0500

    uninstalled nodemon

--- scripts.js
+++ scripts.js
@@ -141,26 +141,26 @@
 // }
 
 
-window.addEventListener("wheel", event=>{
-  let myCamera = document.getElementById("camera");
-  const delta=Math.sign(event.wheelDelta);
-  const currentZoom = Number(myCamera.getAttribute("zoom"));
-  const zoomRate = 0.01;
-  let newZoom = delta * zoomRate + currentZoom;
+// window.addEventListener("wheel", event=>{
+//   let myCamera = document.getElementById("camera");
+//   const delta=Math.sign(event.wheelDelta);
+//   const currentZoom = Number(myCamera.getAttribute("zoom"));
+//   const zoomRate = 0.01;
+//   let newZoom = delta * zoomRate + currentZoom;
  
-  const closestZoom = 5; 
-  const farthestZoom =0.01;
+//   const closestZoom = 5; 
+//   const farthestZoom =0.01;
 
-  if (newZoom > closestZoom){
-      newZoom= closestZoom
-  }
-  if (newZoom < farthestZoom){
-      newZoom = farthestZoom
-  }
+//   if (newZoom > closestZoom){
+//       newZoom= closestZoom
+//   }
+//   if (newZoom < farthestZoom){
+//       newZoom = farthestZoom
+//   }
 
-  myCamera.setAttribute("zoom", newZoom);
-  console.log({newZoom})
-})
+//   myCamera.setAttribute("zoom", newZoom);
+//   console.log({newZoom})
+// })
 
 
 var insideHomescreen=true;
